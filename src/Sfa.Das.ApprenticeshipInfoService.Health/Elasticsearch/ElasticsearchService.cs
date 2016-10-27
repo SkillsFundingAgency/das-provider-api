01d397e6a130c356a4f46e400b5f406c6e66c814
Scott Cowan <info@scottcowan.org>
2016-10-27 12:22:17 +0100

    fixing critical warnings from sonarqube

--- src/Sfa.Das.ApprenticeshipInfoService.Health/Elasticsearch/ElasticsearchService.cs
+++ src/Sfa.Das.ApprenticeshipInfoService.Health/Elasticsearch/ElasticsearchService.cs
@@ -24,67 +24,71 @@
 
         public ElasticsearchLog GetErrorLogs(IEnumerable<Uri> uriStrings, string environment)
         {
-            var connectionSettings = new ConnectionSettings(new StaticConnectionPool(uriStrings));
-            connectionSettings.DisableDirectStreaming();
-            var elasticClient = new ElasticClient(connectionSettings);
-
-            var oneDayAgo = DateMath.Now.Subtract(new Time(new TimeSpan(1, 0, 0, 0)));
-            var result = elasticClient.Search<ElasticsearchLogHit>(
-                s => s
-                .Index("logstash-*")
-                .Type(Types.All)
-                .Query(q => q
-                    .Bool(b => b
-                        .Must(m => m
-                            .Match(ma => ma
-                                .Field("Application")
-                                .Query("search-index"))
-                            , bs2 => bs2
-                            .Match(ma => ma
-                                .Field(f => f.Level)
-                                .Query("error"))
-                             , bs2 => bs2
-                             .Match(ma => ma
-                                .Field("Environment")
-                                .Query(environment))
-                             , bs2 => bs2
-                             .DateRange(r => r.GreaterThan(oneDayAgo).Field("@timestamp")))
-                      )
-                  )
-                );
-            return new ElasticsearchLog
+            using (var connectionSettings = new ConnectionSettings(new StaticConnectionPool(uriStrings)))
             {
-                LogErrors = result.Hits.Select(m => m.Source.Message),
-                ErrorCount = result.Total
-            };
+                connectionSettings.DisableDirectStreaming();
+                var elasticClient = new ElasticClient(connectionSettings);
+
+                var oneDayAgo = DateMath.Now.Subtract(new Time(new TimeSpan(1, 0, 0, 0)));
+                var result = elasticClient.Search<ElasticsearchLogHit>(
+                    s => s
+                        .Index("logstash-*")
+                        .Type(Types.All)
+                        .Query(q => q
+                            .Bool(b => b
+                                .Must(m => m
+                                    .Match(ma => ma
+                                        .Field("Application")
+                                        .Query("search-index"))
+                                    , bs2 => bs2
+                                        .Match(ma => ma
+                                            .Field(f => f.Level)
+                                            .Query("error"))
+                                    , bs2 => bs2
+                                        .Match(ma => ma
+                                            .Field("Environment")
+                                            .Query(environment))
+                                    , bs2 => bs2
+                                        .DateRange(r => r.GreaterThan(oneDayAgo).Field("@timestamp")))
+                            )
+                        )
+                    );
+                return new ElasticsearchLog
+                {
+                    LogErrors = result.Hits.Select(m => m.Source.Message),
+                    ErrorCount = result.Total
+                };
+            }
         }
 
         public ElasticsearchResponse GetElasticHealth(IEnumerable<Uri> uris, string environment)
         {
             try
             {
-                var connectionSettings = new ConnectionSettings(new StaticConnectionPool(uris));
-                var elasticClient = new ElasticClient(connectionSettings);
-                var catAliases = elasticClient
-                    .CatAliases()
-                    .Records
-                    .Where(m => m.Alias.StartsWith(environment));
-                var aliases = new List<ElasticsearchAlias>();
-
-                foreach (var catAlias in catAliases)
+                using (var connectionSettings = new ConnectionSettings(new StaticConnectionPool(uris)))
                 {
-                    var index = elasticClient.CatIndices(m => m.Index(catAlias.Index));
+                    var elasticClient = new ElasticClient(connectionSettings);
+                    var catAliases = elasticClient
+                        .CatAliases()
+                        .Records
+                        .Where(m => m.Alias.StartsWith(environment));
+                    var aliases = new List<ElasticsearchAlias>();
 
-                    aliases.Add(new ElasticsearchAlias
+                    foreach (var catAlias in catAliases)
                     {
-                        AliasName = catAlias.Alias,
-                        IndexName = catAlias.Index,
-                        Status = index.Records.FirstOrDefault()?.Status,
-                        DocumentCount = index.Records.FirstOrDefault()?.DocsCount,
-                        Health = index.Records.FirstOrDefault()?.Health
-                    });
+                        var index = elasticClient.CatIndices(m => m.Index(catAlias.Index));
+
+                        aliases.Add(new ElasticsearchAlias
+                        {
+                            AliasName = catAlias.Alias,
+                            IndexName = catAlias.Index,
+                            Status = index.Records.FirstOrDefault()?.Status,
+                            DocumentCount = index.Records.FirstOrDefault()?.DocsCount,
+                            Health = index.Records.FirstOrDefault()?.Health
+                        });
+                    }
+                    return new ElasticsearchResponse {ElasticsearchAliases = aliases};
                 }
-                return new ElasticsearchResponse { ElasticsearchAliases = aliases };
             }
             catch (Exception exception)
             {
