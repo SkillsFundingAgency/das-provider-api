b28c7e135e76ea241221365294f3f58bfcdd8d78
Scott Cowan <info@scottcowan.org>
2016-10-31 11:34:38 +0000

    FAT-108 adding a version number route to the API

---
+++ src/SFA.DAS.Apprenticeships.Api.Types/Stats/VersionInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Apprenticeships.Api.Types.Stats
+{
+    public class VersionInformation
+    {
+        public string BuildId { get; set; }
+
+        public string Version { get; set; }
+
+        public string AssemblyVersion { get; set; }
+    }
+}
---
+++ src/Sfa.Das.ApprenticeshipInfoService.Api/Controllers/StatsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http.Description;
+
+namespace Sfa.Das.ApprenticeshipInfoService.Api.Controllers
+{
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.Reflection;
+    using System.Web.Http;
+    using SFA.DAS.Apprenticeships.Api.Types.Stats;
+
+    public class StatsController : ApiController
+    {
+        [Route("stats/version")]
+        [HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public VersionInformation Version()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string assemblyInformationalVersion = fileVersionInfo.ProductVersion;
+            return new VersionInformation
+            {
+                BuildId = ConfigurationManager.AppSettings["BuildId"],
+                Version = assemblyInformationalVersion,
+                AssemblyVersion = version
+            };
+        }
+    }
+}
