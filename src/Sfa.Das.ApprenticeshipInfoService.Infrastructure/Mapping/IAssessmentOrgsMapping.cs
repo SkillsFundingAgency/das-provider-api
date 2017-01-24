4289239dab876acfee37d7db97568ef03a96923d
Scott Cowan <info@scottcowan.org>
2017-01-24 12:03:57 +0000

    CI-92 cleaned up using statements

--- src/Sfa.Das.ApprenticeshipInfoService.Infrastructure/Mapping/IAssessmentOrgsMapping.cs
+++ src/Sfa.Das.ApprenticeshipInfoService.Infrastructure/Mapping/IAssessmentOrgsMapping.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections.Generic;
 using Sfa.Das.ApprenticeshipInfoService.Application.Models;
-using SFA.DAS.Apprenticeships.Api.Types;
 using SFA.DAS.Apprenticeships.Api.Types.AssessmentOrgs;
-using Organisation = SFA.DAS.Apprenticeships.Api.Types.AssessmentOrgs.Organisation;
 
 namespace Sfa.Das.ApprenticeshipInfoService.Infrastructure.Mapping
 {
     public interface IAssessmentOrgsMapping
     {
-        OrganisationSummary MapToOrganisationDto(Application.Models.OrganisationDocument organisation);
-        Organisation MapToOrganisationDetailsDto(Application.Models.OrganisationDocument organisation);
+        OrganisationSummary MapToOrganisationDto(OrganisationDocument organisation);
+        Organisation MapToOrganisationDetailsDto(OrganisationDocument organisation);
         IEnumerable<Organisation> MapToOrganisationsDetailsDto(IEnumerable<OrganisationDocument> organisation);
     }
 }
